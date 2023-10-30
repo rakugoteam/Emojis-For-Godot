a097e6cd585f92fc1da6396c3e005b279a89b414
Jeremi Biernacki <jeremi360@gmail.com>
2023-10-30 13:29:15 +0100

    small refactor of LabelWithEmojis.gd

--- addons/emojis-for-godot/examples/LabelWithEmojis.gd
+++ addons/emojis-for-godot/examples/LabelWithEmojis.gd
@@ -4,14 +4,13 @@ extends RichTextLabel
 @export_multiline
 var text_with_emojis: String:
 	set(value):
-		if !Engine.is_editor_hint():
-			await ready
-		_text_with_emojis = value
+		if !is_node_ready():
+			return
+
+		text_with_emojis = value
 		bbcode_enabled = true
 		text = EmojisDB.parse_emojis(value)
 	
 	get:
-		return _text_with_emojis
-
-var _text_with_emojis : String
+		return text_with_emojis
 
